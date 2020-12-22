4f7133a993824ddf9b80db693facf40a59f8a811
Tommaso Tocci <tommaso.tocci@mongodb.com>
2020-12-22 23:49:38 +0000

    Add multi split test

---
+++ jstests/sharding/toto_multi_split.js
@@ -0,0 +1,54 @@
+/*
+ *  Tests that orphans eventually get deleted after setting the featureCompatibilityVersion to 4.4.
+ */
+load("jstests/replsets/rslib.js");
+
+(function() {
+"use strict";
+
+const dbName = "test";
+const collName = "foo";
+const ns = dbName + "." + collName;
+const iterations = 100;
+const slaveDelay = 1;
+
+const st = new ShardingTest({
+    shards: 1,
+    mongos: 1,
+    config: [{rsConfig: {priority: 10}}, {rsConfig: {priority: 0}}, {rsConfig: {priority: 0}}]
+});
+
+jsTest.log("XOXO: Reconfig config replset  ------------------ ");
+var config = st.configRS.getReplSetConfigFromNode();
+printjson(config);
+// Primary
+assert(config.members[0].priority > 0)
+// Secondaries
+assert(config.members[1].priority == 0)
+config.members[1].slaveDelay = slaveDelay;
+assert(config.members[2].priority == 0)
+config.members[2].slaveDelay = slaveDelay;
+
+config.version += 1;
+reconfig(st.configRS, config, true /*force*/);
+
+assert.soon(function() {
+    var secConn = st.configRS.getSecondary();
+    var config = secConn.getDB('local').system.replset.findOne();
+    return config.members[1].slaveDelay == slaveDelay;
+    ;
+});
+
+// Create a sharded collection with four chunks: [-inf, 50), [50, 100), [100, 150) [150, inf)
+assert.commandWorked(st.s.adminCommand({enableSharding: dbName}));
+assert.commandWorked(st.s.adminCommand({movePrimary: dbName, to: st.shard0.shardName}));
+assert.commandWorked(st.s.adminCommand({shardCollection: ns, key: {x: 1}}));
+
+for (var x = 0; x < (50 * 100); x += 50) {
+    jsTest.log("XOXO loop[" + ((x + 50) / 50) + "] Splitting on x: " + x);
+    assert.commandWorked(st.s.adminCommand({split: ns, middle: {x: x}}));
+}
+
+jsTest.log("XOXO stopping ------------------");
+st.stop();
+})();
