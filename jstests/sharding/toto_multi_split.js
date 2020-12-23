0df36ff373d5353882c46bd1eb72aca268197ce2
Tommaso Tocci <tommaso.tocci@mongodb.com>
2020-12-23 13:49:37 +0000

    Split test with 7 config servers

--- jstests/sharding/toto_multi_split.js
+++ jstests/sharding/toto_multi_split.js
@@ -9,16 +9,13 @@ load("jstests/replsets/rslib.js");
 const dbName = "test";
 const collName = "foo";
 const ns = dbName + "." + collName;
-const iterations = 100;
+const iterations = 10000;
 const slaveDelay = 1;
 
-const st = new ShardingTest({
-    shards: 1,
-    mongos: 1,
-    config: [{rsConfig: {priority: 10}}, {rsConfig: {priority: 0}}, {rsConfig: {priority: 0}}]
-});
+const st = new ShardingTest({shards: 1, mongos: 1, config: 7});
 
 jsTest.log("XOXO: Reconfig config replset  ------------------ ");
+/*
 var config = st.configRS.getReplSetConfigFromNode();
 printjson(config);
 // Primary
@@ -30,7 +27,7 @@ assert(config.members[2].priority == 0)
 config.members[2].slaveDelay = slaveDelay;
 
 config.version += 1;
-reconfig(st.configRS, config, true /*force*/);
+reconfig(st.configRS, config, true );
 
 assert.soon(function() {
     var secConn = st.configRS.getSecondary();
@@ -38,13 +35,14 @@ assert.soon(function() {
     return config.members[1].slaveDelay == slaveDelay;
     ;
 });
+*/
 
 // Create a sharded collection with four chunks: [-inf, 50), [50, 100), [100, 150) [150, inf)
 assert.commandWorked(st.s.adminCommand({enableSharding: dbName}));
 assert.commandWorked(st.s.adminCommand({movePrimary: dbName, to: st.shard0.shardName}));
 assert.commandWorked(st.s.adminCommand({shardCollection: ns, key: {x: 1}}));
 
-for (var x = 0; x < (50 * 100); x += 50) {
+for (var x = 0; x < (50 * iterations); x += 50) {
     jsTest.log("XOXO loop[" + ((x + 50) / 50) + "] Splitting on x: " + x);
     assert.commandWorked(st.s.adminCommand({split: ns, middle: {x: x}}));
 }
